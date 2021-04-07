54e03669e9e6f9e12885d57e21f2c3f0c40f2229
kre1e <olesendk@hotmail.com>
2021-04-07 21:43:15 +0200

    123 plat

--- Design-patterns/GameWorld.cs
+++ Design-patterns/GameWorld.cs
@@ -12,6 +12,7 @@ namespace Design_patterns
 
         public SpriteBatch spriteBatch;
         private Texture2D sprite;
+        private Texture2D Platform;
         private List<GameObject> gameobject = new List<GameObject>();
         private Vector2 distance;
         public Vector2 spritePosition;
@@ -48,7 +49,7 @@ namespace Design_patterns
 
             gameobject.Add(EnemyFactory.Instance.Create("Blue"));
             gameobject.Add(PlayerTower.Instance.CreatePlayer());
-
+            gameobject.Add(PlatformPlayer.Instance.CreatePlatformPlayer());
             base.Initialize();
         }
 
@@ -57,6 +58,7 @@ namespace Design_patterns
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Background = Content.Load<Texture2D>("Background");
             sprite = Content.Load<Texture2D>("shield1");
+            Platform = Content.Load<Texture2D>("platform");
             spritePosition = new Vector2(960, 520);
 
         }
